948cec9044f47b2ebf1e71eaebb30705d8a867bd
Sifat Sultan <sifatsultan@gmail.com>
2016-03-15 10:41:36 +0800

    uptime done

--- uptime.sh
+++ uptime.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-uptime | awk '{print $3}' | sed s/.$//
+up=$(uptime | awk '{print $3}' | sed s/.$//)
+IFS=: read -a upArray <<<"$up"
+if [ ${#upArray[@]} -eq 3 ]
+then
+ d=${upArray[0]}
+ h=${upArray[1]}
+ m=${upArray[2]}
+ echo $d" days, "$h" hours, "$m" minutes"
+else
+ h=${upArray[0]}
+ m=${upArray[1]}
+ echo $h" hours, "$m" minutes"
+fi
+
+
+
+
+
 
